53f3135b55a9bcaaef3eb8708160f44a2cf8d182
Kishorkumar28 <kishor28092002@gmail.com>
2024-02-11 20:12:10 +0530

    update

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("Welcome to my repository")
 console.log("Wassup")
+for(let i = 0; i < 5 ;i++){
+    let j=i+1;
+     if (j %2 === 0) {
+         console.log(`I'm number ${i} and I am even`)
+     } else{
+        console.log(`I'm number ${i} and I am odd`);
+     }
+     
+}
+//This is a comment in JavaScript, it won't be executed by the browser but will help you understand what your code does
+//This is a comment in JavaScript, you can see it above this line of code. Comments are ignored by the JavaScript interpreter so they don’t affect how your program runs.
